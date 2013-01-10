7618ccc8281f34da7e5364e46d98a617ddde9248
guillego <ggalan3@gmail.com>
2013-01-10 22:27:16 +0100

    new file:   progresion.php

---
+++ progresion.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Trabajo SALT</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<?php
+foreach (range(1900, 2011) as $number) {
+    echo $number."<br />";
+    echo "<img src=img2/".$number.".png><br />";
+}
+?>
+</body>
+</html>
